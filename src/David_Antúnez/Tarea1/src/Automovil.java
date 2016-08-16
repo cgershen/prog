7b468bceabf8c8c3dda47cd0c388a7433f97046f
Mudvayne084 <c16_davidk4@hotmail.com>
2016-08-16 15:32:24 -0500

    Tarea1
    Herencia y objetos

---
+++ src/David_Antúnez/Tarea1/src/Automovil.java
@@ -0,0 +1,23 @@
+
+public class Automovil 
+{
+    
+    public String marca;
+    public String tipo;
+    public int cilindros;
+    public String transmision;
+    public String motor;
+    public int potencia;
+    
+    public Automovil(String Marca, String Tipo, int Cilindros, String Transmision, String Motor, int Potencia)
+    {
+        this.marca = Marca;
+        this.tipo = Tipo;
+        this.cilindros= Cilindros;
+        this.transmision = Transmision;
+        this.motor = Motor;
+        this.potencia = Potencia;
+        
+    }
+       
+}
