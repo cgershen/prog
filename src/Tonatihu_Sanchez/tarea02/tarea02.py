c86009b31c37f796e53700732fbc0d678251d6a2
tonatihus <tonatihus@users.noreply.github.com>
2016-08-25 03:08:21 -0500

    Add files via upload

---
+++ src/Tonatihu_Sanchez/tarea02/tarea02.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import itertools 
+
+def fibonacci():
+    a,b = 0,1
+    while True:
+        yield a
+        b = a+b
+        yield b
+        a = a+b
+
+print("Suceción de Fibonacci\n")
+
+generador = fibonacci()
+
+try: 
+    cuantos = int(raw_input('¿Cuántos elementos de la serie deseas imprimir? '))
+
+    for x in itertools.islice(generador, cuantos):
+        print x
+
+except ValueError:
+    print("Es imprescindible que escribas un número mayor a cero\n")
+
+except:
+    print("Error inesperado:", sys.exc_info()[0])
+    raise
+
+siguiente =''
+while (siguiente != 'n'):
+	print next(generador)
+	siguiente = raw_input("¿Deseas obtener otro elemento? [S/n]")
