f20dc67084239383e5ee2780336c4a660c99942d
Derek <derek@port7e.ca>
2016-11-15 01:15:59 -0500

    multiple layers

--- back/path_test.py
+++ back/path_test.py
@@ -9,7 +9,7 @@ def sendRequest(a_x, a_y, b_x, b_y):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('127.0.0.1', 8011))
 
-    message = "%s %s %s %s" % (a_x, a_y, b_x, b_y)
+    message = "2 %s %s %s %s" % (a_x, a_y, b_x, b_y)
 
     path_server.replyWith(sock, message)
     reply = path_server.recieveMessage(sock)
