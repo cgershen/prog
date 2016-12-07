e8ed7dc7ae92dd3ec8cc01360a7c45dd9a8f0573
Ubuntu <ubuntu@ip-172-31-26-133.us-west-2.compute.internal>
2016-12-07 04:33:55 +0000

    migrate to full postgres

--- back/pumarideapi/pumaride/settings.py
+++ back/pumarideapi/pumaride/settings.py
@@ -94,9 +94,16 @@ WSGI_APPLICATION = 'pumaride.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pumaride',
+        'USER': 'pumaride',
+        'PASSWORD': 'puma_ride123.',
+	'HOST': 'localhost'
     }
 }
 
