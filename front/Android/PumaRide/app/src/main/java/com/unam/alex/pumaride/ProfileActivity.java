a6c5775627480793bcb6afd776bb166cfa11bd4b
RobertoAmbriz <ing.roberto.ambriz@gmail.com>
2016-12-10 21:15:58 -0600

    Update ProfileActivity.java

--- front/Android/PumaRide/app/src/main/java/com/unam/alex/pumaride/ProfileActivity.java
+++ front/Android/PumaRide/app/src/main/java/com/unam/alex/pumaride/ProfileActivity.java
@@ -1,13 +1,100 @@
 package com.unam.alex.pumaride;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
+    @BindView(R.id.activity_profile_photo)
+    ImageView photoProfile;
+    final private String imageResourseName = "MyImage";
+    private String imagePath;
+    Uri selectedImageUri;
+
+    private static final int SELECT_PICTURE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+
+        photoProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageChooser();
+            }
+        });
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        if (selectedImageUri != null) {
+            imagePath = selectedImageUri.toString();
+            savedInstanceState.putString(imageResourseName, imagePath);
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+
+        imagePath = savedInstanceState.getString(imageResourseName);
+        if(imagePath != null){
+            selectedImageUri = Uri.parse(imagePath);
+            photoProfile.setImageURI(selectedImageUri);
+        }
+    }
+
+    /* Choose an image from Gallery */
+    void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url from data
+                selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // Get the path from the Uri
+                    //imagePath = getPathFromURI(selectedImageUri);
+                    //Log.i(TAG, "Image Path : " + path);
+                    // Set the image in ImageView
+                    photoProfile.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+
+    /* Get the real path from the URI */
+    public String getPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
+    }
+
 }
