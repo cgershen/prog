0db1e2a5d25b4bdae2629f2e62b31cea8e4aa92e
tonatihus <tonatihus@users.noreply.github.com>
2017-01-05 21:28:57 -0600

    Add files via upload

---
+++ back/autenticacion/pumaride_v1.0/sesiones/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from authemail2.admin import EmailUserAdmin
+
+class PumaAdmin(EmailUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff',
+                                       'is_superuser', 'is_verified',
+                                       'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Custom info', {'fields': ('fecha_de_nacimiento',)}),
+    )
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), PumaAdmin)
