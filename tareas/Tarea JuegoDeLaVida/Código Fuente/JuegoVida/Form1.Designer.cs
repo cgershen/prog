26623dee015b678fdad3c188c44394745ca07428
DanielRG007 <42196419+DanielRG007@users.noreply.github.com>
2018-08-16 23:51:07 -0500

    Add files via upload
    Hola a todos, subo mi tarea. Saludos

---
+++ tareas/Tarea JuegoDeLaVida/Código Fuente/JuegoVida/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace JuegoVida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 267);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Poblar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(665, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reiniciar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(615, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ciclo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(592, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(537, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Inserte Densidad de la Matriz ( 1% - 99% )";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(612, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Densidad:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 342);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Juego de la Vida";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ tareas/Tarea JuegoDeLaVida/Código Fuente/JuegoVida/Program.cs
@@ -0,0 +1,126 @@
+﻿//Juego de la vida por Daniel Reyes Guillen
+//Clase de Programación Avanzada Grupo 0401
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using JuegoVida;
+ 
+    public class Program
+    {
+        private int[,] Matriz;    
+        private Point limite;     
+         
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public int[,] BuscaMatriz()
+        {
+            return Matriz;
+        }
+
+        public void JuegoVida(int maxX, int maxY)
+        {
+            Matriz = new int[maxX + 1, maxY + 1];
+            limite = new Point(maxX, maxY); 
+        }
+       
+        public void LLenarMatriz(byte lleno)
+        {
+            Random r = new Random();
+
+            if (lleno > 99 | lleno < 1)
+                return;
+            for (int x = 0; x <= Matriz.GetUpperBound(0) - 1; x++)
+            {
+                for (int y = 0; y <= Matriz.GetUpperBound(1) - 1; y++)
+                {
+                    int i = r.Next(1, 99);
+                    if ( i < lleno)
+                        Matriz[x, y] = 1;
+                    else
+                        Matriz[x, y] = 0;
+                }
+            }
+        }
+
+        public void Iniciar()
+        {
+            int Vivos;
+            int[,] Matriz2 = new int[limite.X + 1, limite.Y + 1];
+
+            for (int x = 0; x <= Matriz.GetUpperBound(0) - 1; x++)
+            { for (int y = 0; y <= Matriz.GetUpperBound(1) - 1; y++)
+                {
+                Vivos = EstanVivos(new Point(x, y));
+                    if (Matriz[x, y] == 0)
+                    {
+                        if (Vivos == 3)
+                            Matriz2[x, y] = 1; 
+                    }
+                    else if (Matriz[x, y] == 1)
+                    {
+                        if (Vivos == 2 || Vivos == 3)
+                            Matriz2[x, y] = 1;
+                    }
+                }
+            }
+
+            Matriz = Matriz2; 
+        }
+     
+        private int EstanVivos(Point vecino)
+        {
+            List<Point> Vecinos = ListaVecinos(vecino);
+            int Vivos = 0;
+            foreach (Point p in Vecinos)
+                Vivos += Matriz[p.X, p.Y];
+            return Vivos;
+        }
+
+        private List<Point> ListaVecinos(Point vecino)
+        {
+            List<Point> Vecinos = new List<Point>();
+            {
+                var posicion = vecino;
+                if (posicion.X > 0 & posicion.Y > 0)
+                    Vecinos.Add(new Point(posicion.X - 1, posicion.Y - 1));
+                if (posicion.Y > 0)
+                    Vecinos.Add(new Point(posicion.X, posicion.Y - 1));
+                if (posicion.X < limite.X & posicion.Y > 0)
+                    Vecinos.Add(new Point(posicion.X + 1, posicion.Y - 1));
+                if (posicion.X > 0)
+                    Vecinos.Add(new Point(posicion.X - 1, posicion.Y));
+                if (posicion.X < limite.X)
+                    Vecinos.Add(new Point(posicion.X + 1, posicion.Y));
+                if (posicion.X > 0 & posicion.Y < limite.Y)
+                    Vecinos.Add(new Point(posicion.X - 1, posicion.Y + 1));
+                if (posicion.Y < limite.Y)
+                    Vecinos.Add(new Point(posicion.X, posicion.Y + 1));
+                if (posicion.X < limite.X & posicion.Y < limite.Y)
+                    Vecinos.Add(new Point(posicion.X + 1, posicion.Y + 1));
+            }
+
+            return Vecinos;
+        }
+
+        public float Densidad()
+        {
+            int a = 0;
+            for (int x = 0; x <= Matriz.GetUpperBound(0) - 1; x++)
+            {
+                for (int y = 0; y <= Matriz.GetUpperBound(1) - 1; y++)
+                {
+                    if (Matriz[x, y] == 1)
+                        a += 1;
+                }
+            }
+            return a / (float)(Matriz.GetUpperBound(0) * Matriz.GetUpperBound(1));
+        }
+
+    }
+
